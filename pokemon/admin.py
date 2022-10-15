6b81f6376331615814e2b9f186b002f5a368a4aa
BLoushi52 <k.albloushii@hotmail.com>
2022-10-15 19:24:11 +0300

    updated admin page

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from django.contrib import admin
 
 from pokemon.models import Pokemon
@@ -12,3 +13,29 @@ class PokemonAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "hp", "active",)
     list_display_links = ("id", "name",)
     list_filter = ("active",)
+    readonly_fields = ("created_at", "modified_at")
+
+    fieldsets = (
+        (
+            "general", {
+                "fields": ("name", "hp", "active", "type",)
+            }
+        ),
+        (
+            "Localizations", {
+                "fields": ("name_ar", "name_fr", "name_jp",),
+                "classes": ("collapse",)
+            }
+        ),
+        (
+            None, {
+                "fields": ("created_at", "modified_at",)
+                }
+        ),
+
+
+
+
+
+
+    )
