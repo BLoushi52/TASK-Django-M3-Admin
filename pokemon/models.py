4ba5227681d4ad4f5037d5177031d9b7d2cbbd8d
BLoushi52 <k.albloushii@hotmail.com>
2022-10-15 19:05:41 +0300

    added Validators

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,4 +1,6 @@
+from wsgiref.validate import validator
 from django.db import models
+from django.core.validators import (MinValueValidator, MaxValueValidator)
 
 # Create your models here.
 
@@ -11,12 +13,12 @@ class Pokemon(models.Model):
        FAIRY = "FA"
 
     name = models.CharField(max_length=30)
-    type = models.CharField(max_length=30, choices=PokemonType.choices)
-    hp = models.PositiveIntegerField()
+    type = models.CharField(max_length=2, choices=PokemonType.choices)
+    hp = models.PositiveIntegerField(validators=[MinValueValidator(50, "Min Value is 50"), MaxValueValidator(350, "Max Value is 350")])
     active  = models.BooleanField(default=True)
-    name_fr = models.CharField(max_length=30, default="")
-    name_ar = models.CharField(max_length=30, default="")
-    name_jp = models.CharField(max_length=30, default="")
+    name_fr = models.CharField(max_length=30, default="", blank=True)
+    name_ar = models.CharField(max_length=30, default="", blank=True)
+    name_jp = models.CharField(max_length=30, default="", blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
